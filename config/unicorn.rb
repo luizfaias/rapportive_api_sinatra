f7e940d14374113781d6dcdbf256683c5319a3fd
Bruno Ghisi <brunogh@gmail.com>
2014-06-22 10:32:21 -0300

    config unicorn init

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,14 +7,10 @@ before_fork do |server, worker|
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
   end
-  Resque.redis.quit
-  ActiveRecord::Base.connection.disconnect! if defined?(ActiveRecord::Base)
 end
 
 after_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
   end
-  Resque.redis = $redis
-  ActiveRecord::Base.establish_connection if defined?(ActiveRecord::Base)
 end
